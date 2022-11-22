714b019c808af358d47f736f77d7450307046f07
Denis Yakovlev <d.yakovlev@leningrad.io>
2022-11-22 14:08:01 +0400

    Add fix to before window maximize

--- home-work-17.test.js
+++ home-work-17.test.js
@@ -5,22 +5,21 @@ const assert = require("assert");
 suite(function(env) {
     describe('Tests for chromedriver.chromium.org', function() {
         let driver;
-        
+
         before(async function() {
             driver = await new Builder().forBrowser('chrome').build();
+            await driver.manage().window().maximize();
         });
 
         after(async () => await driver.quit());
 
         it('Test #1.1. Check title for /home', async function() {
-            await driver.manage().window().maximize();
             await driver.get('https://chromedriver.chromium.org/home');
             const value = await driver.getTitle()
             assert.equal("ChromeDriver - WebDriver for Chrome", value);
         });
 
         it('Test #1.2. Check title for /extensions', async function() {
-            await driver.manage().window().maximize();
             await driver.get('https://chromedriver.chromium.org/home');
             await driver.findElement(By.xpath('//*[@id="WDxLfe"]/ul/li[3]/div[1]/div/a')).click();
             const value = await driver.getTitle()
@@ -28,7 +27,6 @@ suite(function(env) {
         });
 
         it('Test #2. Check search url contains word driver', async function() {
-            await driver.manage().window().maximize();
             await driver.get('https://chromedriver.chromium.org/home');
             await driver.wait(until.elementLocated(By.xpath('//*[@id="atIdViewHeader"]/div/div[2]/div[1]/div[2]/div/span/span'))).click();
             await driver.wait(until.elementLocated(By.xpath('//*[@id="yDmH0d"]/div[1]/div/div[1]/div[2]/div[3]/div[2]/div/div[1]/div/div[1]/input'))).sendKeys('Driver');
@@ -38,7 +36,6 @@ suite(function(env) {
         });
 
         it('Test #3. Check mobile-emulation url contains /mobile-emulation', async function() {
-            await driver.manage().window().maximize();
             await driver.get('https://chromedriver.chromium.org/home');
             await driver.wait(until.elementLocated(By.xpath('//*[@id="WDxLfe"]/ul/li[12]/div[1]/div/a'))).click();
             await driver.wait(until.elementLocated(By.xpath('//*[@id="WDxLfe"]/ul/li[12]/div[2]/ul/li[9]/div/div/a'))).click();
